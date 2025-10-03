bba289b233ba62d9741f4529cb46831309b68a39
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-02 20:36:49 -0400

    added varibles and trying to add a loop needs work

--- src/main/java/com/pluralsight/Dice.java
+++ src/main/java/com/pluralsight/Dice.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class Dice {
 
+    //A Method that rolls the "Dice" and outputs a random number from 6-1
     public int roll (){
         int randomNumber = (int)(Math.random()* 6)+1;
         return randomNumber;
--- src/main/java/com/pluralsight/Main.java
+++ src/main/java/com/pluralsight/Main.java
@@ -4,12 +4,28 @@ public class Main {
     public static void main(String[] args) {
 
         //Creation of Dice
-        Dice dice1 = new Dice();
-        Dice dice2 = new Dice();
+        Dice dice = new Dice();
 
 
-        int roll1 = dice1.roll();
-        int roll2 = dice2.roll();
-        
+        //Int for roll variables
+        int roll1;
+        int roll2;
+
+        //Counters for number 2,4,6,7(weird choice of numbers)
+        int count2 = 0;
+        int count4 = 0;
+        int count6 = 0;
+        int count7 = 0;
+
+        //For loop that executes 100 times
+        int i = 0;
+
+        while(i>=100){
+            roll1 = dice.roll();
+            roll2 = dice.roll();
+            i++;
+        }
+
+
     }
 }
