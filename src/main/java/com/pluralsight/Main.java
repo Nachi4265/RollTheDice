9005678f32b637ba7878c36f1397bd323f7801be
Nachi4265 <hvirgen@my.yearupunited.org>
2025-10-02 20:06:41 -0400

    Added Dice and made a roll method

---
+++ src/main/java/com/pluralsight/Main.java
@@ -0,0 +1,15 @@
+package com.pluralsight;
+
+public class Main {
+    public static void main(String[] args) {
+
+        //Creation of Dice
+        Dice dice1 = new Dice();
+        Dice dice2 = new Dice();
+
+
+        int roll1 = dice1.roll();
+        int roll2 = dice2.roll();
+        
+    }
+}
